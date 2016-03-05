47a7ad4530929ac846c16c87b10a3b22c2e2ee57
karimsamir <karim5977@gmail.com>
2016-03-06 00:09:54 +0200

    finishing the chapter 7 by applying checkout

--- angularjs/controllers/sportsStore.js
+++ angularjs/controllers/sportsStore.js
@@ -1,6 +1,8 @@
 angular.module("sportsStore")
         .constant("dataUrl", "http://localhost:5000/products")
-        .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+        .constant("orderUrl", "http://localhost:5000/orders")
+        .controller("sportsStoreCtrl", function ($scope, $http, $location,
+                dataUrl, orderUrl, cart) {
 
 //    $scope.data = {
 //        products: [
@@ -22,5 +24,27 @@ angular.module("sportsStore")
                     })
                     .error(function (error) {
                         $scope.data.error = error;
-                    })
+                    });
+
+
+            $scope.sendOrder = function (shippingDetails) {
+                var order = angular.copy(shippingDetails);
+                order.products = cart.getProducts();
+
+                $http.post(orderUrl, order)
+                        .success(function (data) {
+                            $scope.data.orderId = data.id;
+                            cart.getProducts().length = 0;
+                        })
+                        .error(function (error) {
+                            $scope.data.orderError = error;
+                        })
+                        .finally(function () {
+                            $location.path("/complete");
+                        });
+            };
         });
+
+
+
+
