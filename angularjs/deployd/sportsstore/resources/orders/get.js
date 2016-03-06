b11d7eb5473c1845c2b143f80f92ecb5321aa7c6
karimsamir <karim5977@gmail.com>
2016-03-06 17:28:42 +0200

    finishing the chapter 7 by applying checkout

--- angularjs/deployd/sportsstore/resources/orders/get.js
+++ angularjs/deployd/sportsstore/resources/orders/get.js
@@ -1,3 +1,3 @@
-if (me === undefined || me.username != "admin") {
-    cancel("No authorization", 401);
-}
+// if (me === undefined || me.username != "admin") {
+//     cancel("No authorization", 401);
+// }
